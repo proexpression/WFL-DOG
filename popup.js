d82e1f29b79d45b273118fd4712ec5908d51ee08
proexpression <proexpression93@gmail.com>
2020-01-16 21:11:10 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -23,7 +23,44 @@ function uncheckAllStat() {
     
 } 
 
-document.getElementById("btn1").addEventListener("click", function() {
-    chrome.tabs.executeScript({"code": 'document.getElementById("body-container").style.backgroundColor = "blue";'});
-}, false);
+function checkAllPools() { 
+    var inputs = document.querySelectorAll('.pool'); 
+    for (var i = 0; i < inputs.length; i++) { 
+        inputs[i].checked = true; 
+    } 
+}
+    
+//create uncheckAllLdaps function 
+function uncheckAllPools() { 
+    var inputs = document.querySelectorAll('.pool'); 
+    for (var i = 0; i < inputs.length; i++) { 
+        inputs[i].checked = false; 
+    } 
+} 
+
+function myFunction() {
+    var coffee = document.forms[0];
+    var txt = "";
+    var i;
+    for (i = 0; i < coffee.length; i++) {
+      if (coffee[i].checked) {
+        txt = txt + coffee[i].value + " ";
+      }
+    }
+    document.getElementById("order").value = txt;
+  }
+
+  function copyFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("order");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    
+  }
         
